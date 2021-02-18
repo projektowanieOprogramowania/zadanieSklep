6a1bba8352e181f7619a9a10fdcede959b24664d
Jakub <jakubwiton22@gmail.com>
2021-02-18 10:39:02 +0100

    Dodano obliczanie wieku

--- zadanie.py
+++ zadanie.py
@@ -1,5 +1,8 @@
 def czyPelnoletni(rokUrodzenia):
     if 2021-rokUrodzenia >= 18:
-     return True
+        return True
     else:
-    return False
+        return False
+        
+wiek = 2021 - int(input())
+print(wiek)
